a13e899af986d43032b66a92f347a0352a7d266f
Vigneshwar K R <vicky.pcbasic@gmail.com>
2020-09-16 20:30:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ output = gTTS(text=s,lang = language,slow = False)
 output.save("output.mp3")
 opt = int(input("Enter 0 for saving the audio file and Enter 1 for playing back live"))
 if opt == 0:
-    .export('output.mp3', format='mp3')
+    output.export('output.mp3', format='mp3')
 elif opt == 1:
     playsound("output.mp3")
 
