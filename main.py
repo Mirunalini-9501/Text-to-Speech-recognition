851cebd5e35e5356d6b3a07305cca801c62ddd84
Mirunalini S <71172656+Mirunalini-9501@users.noreply.github.com>
2020-09-16 20:29:42 +0530

    Changed the code
    Add some extra features to get the code complete!!!

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import gtts
-import playsound as ps
+from gtts import gTTS
+from playsound import playsound
+from pydub import AudioSegment
+s = str(input())
+language = 'en'
+output = gTTS(text=s,lang = language,slow = False)
+output.save("output.mp3")
+opt = int(input("Enter 0 for saving the audio file and Enter 1 for playing back live"))
+if opt == 0:
+    .export('output.mp3', format='mp3')
+elif opt == 1:
+    playsound("output.mp3")
 
-print("Hello world")
+
 
